df3ac04265d079b9b0b5a1395c0d3d4d98a4cb5e
Gabriel Lievano <glievano@pinterest.com>
2022-05-24 13:20:27 -0700

    Add Fastfile to ruby-mode

--- gamma/modules/ruby.el
+++ gamma/modules/ruby.el
@@ -5,7 +5,7 @@
          ("\\.gemspec\\'" . ruby-mode)
          ("\\.ru\\'" . ruby-mode)
          ("(Release)?Fastmail\\'" . ruby-mode)
-         ("(App|Cap|Cart|Gem|Guard|Match|Pod|Rake)file\\'" . ruby-mode)
+         ("(App|Cap|Cart|Fast|Gem|Guard|Match|Pod|Rake)file\\'" . ruby-mode)
          ("Vagrantfile(.local)\\'" . ruby-mode)))
 
 ;;; ruby.el ends here
